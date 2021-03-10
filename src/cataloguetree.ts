3c8bcc6e756cb5fbb9ae638ca9a664298554e964
barykrg <barykrg@gmail.com>
2021-03-11 01:18:14 +0530

    RI dummy

--- src/cataloguetree.ts
+++ src/cataloguetree.ts
@@ -21,14 +21,9 @@ export class DepNodeProvider implements vscode.TreeDataProvider<Dependency> {
 
 	getChildren(element?: Dependency): vscode.ProviderResult<Dependency[]>{
 		
-		//this.data=images;
-		//return element?Promise.resolve([]):Promise.resolve(this.generate());
-		//let data = JSON.parse(fs.readFileSync("src/file.json", 'utf-8'));
 		const images:Dependency[]=[];
-		images.push(new Dependency("","Reference Implementations","","",this.getRI()));
+		images.push(new Dependency("","Reference Implementation","","",this.getRI()));
 		images.push(new Dependency("","Sdks","","",this.getdks()));
-		//images.push(this.getdks());
-		//console.log(images);
 		return element===undefined?images:element.children;
 
 	}
@@ -36,10 +31,8 @@ export class DepNodeProvider implements vscode.TreeDataProvider<Dependency> {
 	{
 		let data = inputs.global;
 		const sdks:Dependency[]=[];
-		//console.log(data.sdk);
 		for(let sd of data.sdk)
 		{
-			//console.log(sd.name+" "+sd.label+" "+sd.documentation);
 			sdks.push(new Dependency(sd.name,sd.label,sd.documentation,"Image"));
 		}
 		return sdks.length===0?undefined:sdks;
@@ -48,10 +41,22 @@ export class DepNodeProvider implements vscode.TreeDataProvider<Dependency> {
 	{
 		let data = inputs.global;
 		const ris:Dependency[]=[];
-		//console.log(data.sdk);
+		let map = new Map();
 		for(let ri of data.reference_Implementations)
 		{
-			ris.push(new Dependency(ri.name,ri.label,ri.documentation,"Image"));
+			if(!map.has(ri.name.en))
+			{
+				map.set(ri.name.en,[ri.image]);
+			}
+			else
+			{
+				map.set(ri.name.en,map.get(ri.name.en).concat([ri.image]));
+			}
+		}
+		for(let i of map.keys())
+		{
+			let risImage:Dependency[]=[];
+			ris.push(new Dependency(map.get(i).join(","),i,"https://www.google.com","Image"));
 		}
 		return ris.length===0?undefined:ris;
 	}
@@ -59,6 +64,11 @@ export class DepNodeProvider implements vscode.TreeDataProvider<Dependency> {
 	{
 		//vscode.window.showInformationMessage(item.buildCommand);
 		//vscode.window.showInformationMessage(`docker pull ${item.name}`);
+		if(item.name.split(',').length>1)
+		{
+			vscode.window.showInformationMessage(`This is a dummy Pull of ${item.name}`);
+			return;
+		}
 		child.exec(`docker pull ${item.name}`,(error)=>{
 			if(error)
 			{
--- src/imagetree.ts
+++ src/imagetree.ts
@@ -24,10 +24,11 @@ export class DepNodeProvider implements vscode.TreeDataProvider<Dependency> {
 	 getChildren(element?: Dependency): vscode.ProviderResult<Dependency[]> {
 		const images:Dependency[]=[];
 		const sdks = this.getsdks();
-		//const ris = this.getris();
+		const ris = this.getris();
 
+		
+		images.push(new Dependency("Reference Implementation","ris",{},ris));
 		images.push(new Dependency("Sdks","sdks",{},sdks));
-		//images.push(new Dependency("Reference Implementation","ris",{},ris));
 		
 		return element===undefined?images:element.children;
 	}
@@ -36,6 +37,33 @@ export class DepNodeProvider implements vscode.TreeDataProvider<Dependency> {
 		let term =vscode.window.createTerminal("Demo");
 		term.show(true);
 		term.sendText(item.metaData.documentation);
+	}
+	getris():Dependency[]
+	{
+		let data = inputs.global;
+		const ris:Dependency[]=[];
+		let map = new Map();
+		for(let ri of data.reference_Implementations)
+		{
+			if(!map.has(ri.name.en))
+			{
+				map.set(ri.name.en,[ri.image]);
+			}
+			else
+			{
+				map.set(ri.name.en,map.get(ri.name.en).concat([ri.image]));
+			}
+		}
+		for(let i of map.keys())
+		{
+			let risImage:Dependency[]=[];
+			for(let j of map.get(i))
+			{
+				risImage.push(new Dependency(j,"ris"));
+			}
+			ris.push(new Dependency(i,"image",{},[...risImage]));
+		}
+		return ris;
 	}
 	getsdks():Dependency[]
 	{
@@ -60,13 +88,15 @@ export class DepNodeProvider implements vscode.TreeDataProvider<Dependency> {
 					sdksSampleFolder.push(new Dependency(j.substr(j.lastIndexOf('/')+1),"sampleFolder",{},[...sdksSample]));
 				}
 				let sdksDemoFolder:Dependency[]=[];
+				let sdksSampleDirectory:Dependency[]=[];
 				let sdksDemo:Dependency[]=[];
 				for(let j of i.demos)
 				{
 					sdksDemo.push(new Dependency(j.name,"demo",{documentation:j.command}));
 				}
-				sdksDemoFolder.push(new Dependency("Demos","DemoFolder",{},[...sdksDemo]));
-				sdksDemoFolder=sdksDemoFolder.concat(sdksSampleFolder);
+				sdksDemoFolder.push(new Dependency("Demos","demoFolder",{},[...sdksDemo]));
+				sdksSampleDirectory.push(new Dependency("Samples","sampleFolder",{},[...sdksSampleFolder]));
+				sdksDemoFolder=sdksDemoFolder.concat(sdksSampleDirectory);
 				sdks.push(new Dependency(i.label,"image",{documentation:i.documentation,path:"",name:i.name},[...sdksDemoFolder]));
 			}
 		}
@@ -78,7 +108,6 @@ export class DepNodeProvider implements vscode.TreeDataProvider<Dependency> {
 		const m = {"c":["common","hello_classification","hello_nv12_input_classification","object_detection_sample_ssd"],"cpp":["benchmark_app","classification_sample_async","common","hello_classification","hello_nv12_input_classification","hello_reshape_ssd"],"python":["classification_sample_async","hello_classification","hello_query_device","object_detection_sample_ssd","ngraph_function_creation_sample"]};
 		 //let test = child.execSync(`docker run --rm ${name} ls -F ${path}`);
 		 
-		 //let test = "common/ \nhelloWorld/";
 		 /*
 		 let arr:string []= test.toString().split('\n').filter((item)=>{
 				return item.endsWith('/');
@@ -88,16 +117,19 @@ export class DepNodeProvider implements vscode.TreeDataProvider<Dependency> {
 		return arr;
 		*/
 		let parent = path.substr(path.lastIndexOf('/')+1);
-		//console.log(parent);
-		//console.log(m[parent]);
 		let arr = m[parent].map((data)=>{ return path+"/"+data});
-		//console.log(arr);		
+	
 		return arr;
 
 	}
 	
 	public createEnvironment(item:Dependency)
 	{
+		if(item.metaData.name===undefined)
+		{
+			vscode.window.showInformationMessage("Dummy Run");
+			return;
+		}
 		vscode.window.showOpenDialog({canSelectFolders:true})
 		.then(folder =>
 		{
@@ -118,14 +150,7 @@ export class DepNodeProvider implements vscode.TreeDataProvider<Dependency> {
 		.then(undefined,err =>{vscode.window.showErrorMessage("Error while opening the folder"); return;});
 	}
 	public add(item:Dependency)
-	{
-		
-		
-		//console.log(arr);
-		//console.log(`docker exec ${arr[0]} cp -r ${arr[1]}/${item.label} /tmp/${item.parents}`);
-
-		//console.log(`docker exec ${item.metaData.name} cp -r ${item.metaData.path} /tmp`);
-		
+	{	
 		child.exec(`docker exec ${item.metaData.name} cp -r ${item.metaData.path} /tmp`,(error)=>{
 			if(error)
 			{
@@ -137,7 +162,6 @@ export class DepNodeProvider implements vscode.TreeDataProvider<Dependency> {
 	
 	private getString():string[]{
 		return child.execSync(`docker images --format "{{.Repository}}"`,{encoding:"utf8"}).split('\n');
-		//return "";
 	}
 
 }
@@ -161,3 +185,5 @@ export class Dependency extends vscode.TreeItem {
 	}
 
 }
+
+
